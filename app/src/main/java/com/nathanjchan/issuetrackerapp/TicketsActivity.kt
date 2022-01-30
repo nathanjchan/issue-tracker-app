a55f3d7f60b7d0edab73dbbd18835c5961d66bf7
nathanjchan <njchan@ucdavis.edu>
2022-01-29 18:44:07 -0800

    Rename Main activity to Login and create Tickets activity.

---
+++ app/src/main/java/com/nathanjchan/issuetrackerapp/TicketsActivity.kt
@@ -0,0 +1,11 @@
+package com.nathanjchan.issuetrackerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TicketsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tickets)
+    }
+}
