15a4c405d07cbe5f3c55889cb37a413f41f59fa6
nathanjchan <njchan@ucdavis.edu>
2022-01-30 13:44:18 -0800

    Punctuation in comments.

--- app/src/androidTest/java/com/nathanjchan/issuetrackerapp/LoginFragmentTest.kt
+++ app/src/androidTest/java/com/nathanjchan/issuetrackerapp/LoginFragmentTest.kt
@@ -18,28 +18,25 @@ class LoginFragmentTest {
 
     @Test
     fun testNavigationToSignupFragment() {
-        // Create TestNavHostController
+        // Create TestNavHostController.
         val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
-        // Create FragmentScenario for LoginFragment
+        // Create FragmentScenario for LoginFragment.
         val loginScenario = launchFragmentInContainer<LoginFragment>()
 
         loginScenario.onFragment { fragment ->
-            // Set nav graph for TestNavHostController
+            // Set nav graph for TestNavHostController.
             navController.setGraph(R.navigation.nav_graph)
-            // Associate this FragmentScenario with this TestNavHostController
+            // Associate this FragmentScenario with this TestNavHostController.
             Navigation.setViewNavController(fragment.requireView(), navController)
         }
 
-        // Verify that clicking signup button changes NavController's state to signup fragment
+        // Verify that clicking signup button changes NavController's state to Signup Fragment.
         onView(ViewMatchers.withId(R.id.signupButton)).perform(ViewActions.click())
         Assert.assertEquals(navController.currentDestination?.id, R.id.loginFragment)
 
-        // Verify that clicking the back button changes the NavController's state to login fragment
+        // Verify that clicking the back button changes the NavController's state to Login Fragment.
         onView(ViewMatchers.doesNotHaveFocus()).perform(ViewActions.pressBack())
         Assert.assertEquals(navController.currentDestination?.id, R.id.signupFragment)
-
-        // Verify that clicking the back button
-
     }
 
     @Test
@@ -49,10 +46,10 @@ class LoginFragmentTest {
             LoginFragment().also { fragment ->
                 // In addition to returning a new instance of Fragment, get a callback whenever
                 // the fragment's view is created or destroyed so we can set the NavController
-                // ahead of time
+                // ahead of time.
                 fragment.viewLifecycleOwnerLiveData.observeForever{ viewLifecycleOwner ->
                     if (viewLifecycleOwner != null) {
-                        // The fragment's view has just been created
+                        // The fragment's view has just been created.
                         navController.setGraph(R.navigation.nav_graph)
                         Navigation.setViewNavController(fragment.requireView(), navController)
                     }
