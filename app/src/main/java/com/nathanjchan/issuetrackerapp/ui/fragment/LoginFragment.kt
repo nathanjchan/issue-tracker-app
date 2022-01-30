103cc7efd44023e2b16fb648b2a971cdcf05a58e
nathanjchan <njchan@ucdavis.edu>
2022-01-30 01:33:15 -0800

    Add login to signup navigation.

--- app/src/main/java/com/nathanjchan/issuetrackerapp/ui/fragment/LoginFragment.kt
+++ app/src/main/java/com/nathanjchan/issuetrackerapp/ui/fragment/LoginFragment.kt
@@ -5,15 +5,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.nathanjchan.issuetrackerapp.R
+import com.nathanjchan.issuetrackerapp.databinding.FragmentLoginBinding
+import com.nathanjchan.issuetrackerapp.ui.viewmodel.LoginViewModel
 
 class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+    private val viewModel: LoginViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+        binding = FragmentLoginBinding.bind(view)
+
+        binding.loginButton.setOnClickListener {
+            viewModel.attemptLogin()
+        }
+
+        binding.loginToSignupButton.setOnClickListener {
+            val action = LoginFragmentDirections.actionLoginFragmentToSignupFragment()
+            findNavController().navigate(action)
+        }
+
+        return view
     }
 }
---
+++ app/src/main/java/com/nathanjchan/issuetrackerapp/ui/viewmodel/LoginViewModel.kt
@@ -0,0 +1,9 @@
+package com.nathanjchan.issuetrackerapp.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    fun attemptLogin() {
+
+    }
+}
